f519de84f0a829a7963877d6f8b0491e8727bdc8
saviosid <savio_furtado@hotmail.com>
2019-05-20 16:42:10 +0530

    changed process env

--- app.js
+++ app.js
@@ -64,6 +64,6 @@ app.use(indexRoutes);
 const port = process.env.PORT || 3000;
 let ip = process.env.IP || "127.0.0.1";
 
-app.listen(port, ip, () =>
+app.listen(process.env.PORT, process.env.IP, () =>
     console.log(`Yelpcamp Server started on port ${port} ip ${ip}`)
 );
